ccbde3d9a05b042a41b7b98dd37f95c85bb71fa4
AlexNexton <cianoc13@gmail.com>
2021-05-08 19:05:16 +0000

    added an import

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -7,10 +7,8 @@ from .models import Order, OrderLineItem
 from products.models import Product
 from profiles.models import UserProfile
 
-
 import json
 import time
-
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -27,13 +25,13 @@ class StripeWH_Handler:
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-
+        
         send_mail(
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
-        )
+        )        
 
     def handle_event(self, event):
         """
