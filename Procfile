2d6d81eab229e633806fce83f1c94e9422cc7491
AlexNexton <cianoc13@gmail.com>
2021-05-10 21:36:09 +0000

    deployment part 2 - heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn soleki.wsgi:application
--- soleki/settings.py
+++ soleki/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-4fpf3ffpga5o)0jn$nuz+42ag5(q5f^19c&%f$l0u3-fyz)oor
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['soleki.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,24 @@ WSGI_APPLICATION = 'soleki.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
