3b0089e262b0386bb019a28516d657b127974eea
AlexNexton <cianoc13@gmail.com>
2021-05-13 01:37:19 +0000

    added skate detail - 60% complete

--- skateteam/urls.py
+++ skateteam/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.skateteam, name='skateteam'),
+    path('<int:memeber_id>/', views.sdetail, name='sdetail'),
 ]
--- skateteam/views.py
+++ skateteam/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Skateteam
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Skateteam, Teamcat
 
 
 # Create your views here.
@@ -14,3 +14,15 @@ def skateteam(request):
     }
 
     return render(request, 'skateteam/skateteam.html', context)
+
+
+def sdetail(request, memeber_id):
+    """ A view to show individual product details """
+
+    memeber = get_object_or_404(Skateteam, pk=memeber_id)
+
+    context = {
+        'memeber': memeber,
+    }
+
+    return render(request, 'skateteam/sdetail.html', context)
