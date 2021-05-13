a704f41c811cc9efcdda874e0b63b8c1d5905084
AlexNexton <cianoc13@gmail.com>
2021-05-13 10:35:34 +0000

    comment: changed name and added comments to customs models

--- skateteam/models.py
+++ skateteam/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
+# two custon models for new app - code inspired by CI Boutique-ado project
+
 
 class Teamcat(models.Model):
+
+    """
+    Model Teamcat to categorise skaters on the website.
+    """
+
     class Meta:
-        verbose_name_plural = 'Teamcat'  # changes the name of the models
+        verbose_name_plural = 'Teamcat'  # changes the name of the models in django server
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
@@ -16,6 +23,11 @@ class Teamcat(models.Model):
 
 
 class Skateteam(models.Model):
+    """
+    Model Skateteam to gather required information to create
+    a memeber as an item on the site.
+    """
+
     teamcat = models.ForeignKey(
         'Teamcat', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
